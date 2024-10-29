cbc21a7b39cf5c9fdbe30821248e1e0784b2893c
Gary Rong <garyrong0905@gmail.com>
2024-10-29 14:20:47 +0800

    single compaction

--- ethdb/pebble/pebble.go
+++ ethdb/pebble/pebble.go
@@ -20,7 +20,6 @@ package pebble
 import (
 	"bytes"
 	"fmt"
-	"runtime"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -360,7 +359,7 @@ func New(file string, cache int, handles int, namespace string, readonly bool, e
 
 		// The default compaction concurrency(1 thread),
 		// Here use all available CPUs for faster compaction.
-		MaxConcurrentCompactions: runtime.NumCPU,
+		MaxConcurrentCompactions: 1,
 
 		// Per-level options. Options for at least one level must be specified. The
 		// options for the last level are used for all subsequent levels.
