c7724f12d31529215ba9d55f006494caec2051fc
Gary Rong <garyrong0905@gmail.com>
2025-02-19 21:27:49 +0800

    add debug code

--- core/vm/gas.go
+++ core/vm/gas.go
@@ -17,6 +17,7 @@
 package vm
 
 import (
+	"github.com/ethereum/go-ethereum/log"
 	"github.com/holiman/uint256"
 )
 
@@ -36,19 +37,29 @@ const (
 // The cost of gas was changed during the homestead price change HF.
 // As part of EIP 150 (TangerineWhistle), the returned gas is gas - base * 63 / 64.
 func callGas(isEip150 bool, availableGas, base uint64, callCost *uint256.Int) (uint64, error) {
+	var underflow bool
 	if isEip150 {
+		if availableGas < base {
+			underflow = true
+			log.Info("Underflow occurs", "availableGas", availableGas, "base", base)
+		}
 		availableGas = availableGas - base
 		gas := availableGas - availableGas/64
 		// If the bit length exceeds 64 bit we know that the newly calculated "gas" for EIP150
 		// is smaller than the requested amount. Therefore we return the new gas instead
 		// of returning an error.
 		if !callCost.IsUint64() || gas < callCost.Uint64() {
+			if underflow {
+				log.Info("Underflowed gas cost", "result", gas)
+			}
 			return gas, nil
 		}
 	}
 	if !callCost.IsUint64() {
 		return 0, ErrGasUintOverflow
 	}
-
+	if underflow {
+		log.Info("Underflowed gas cost", "result", callCost.Uint64())
+	}
 	return callCost.Uint64(), nil
 }
