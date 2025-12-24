e9965af0b68e407cfaef9fb4fdea0a574dc14396
Gary Rong <garyrong0905@gmail.com>
2025-12-24 14:04:24 +0800

    core: fix format

--- core/blockchain_stats.go
+++ core/blockchain_stats.go
@@ -117,7 +117,9 @@ func (s *ExecuteStats) logSlow(block *types.Block, slowBlockThreshold time.Durat
 Block: %v (%#x) txs: %d, mgasps: %.2f, elapsed: %v
 
 EVM execution: %v
-Validation: %v (%s)
+%s
+
+Validation: %v
 State read: %v
     Account read: %v(%d)
     Storage read: %v(%d)
@@ -135,7 +137,7 @@ DB write: %v
 %s
 ##############################
 `, block.Number(), block.Hash(), len(block.Transactions()), s.MgasPerSecond, common.PrettyDuration(s.TotalTime),
-		common.PrettyDuration(s.Execution), s.EVMStats,
+		common.PrettyDuration(s.Execution), s.EVMStats.String(),
 		common.PrettyDuration(s.Validation+s.CrossValidation),
 
 		// State read
