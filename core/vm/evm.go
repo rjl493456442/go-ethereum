a62684a96e8d1badf38bbbb3ee6287381db66e4a
Gary Rong <garyrong0905@gmail.com>
2025-12-24 14:32:22 +0800

    core/vm: fix precompile

--- core/vm/evm.go
+++ core/vm/evm.go
@@ -411,7 +411,9 @@ func (evm *EVM) CallCode(caller common.Address, addr common.Address, input []byt
 
 	// It is allowed to call precompiles, even via delegatecall
 	if p, isPrecompile := evm.precompile(addr); isPrecompile {
+		ptime := time.Now()
 		ret, gas, err = RunPrecompiledContract(p, input, gas, evm.Config.Tracer)
+		evm.Stats.recordPrecompile(ptime)
 	} else {
 		// Initialise a new contract and set the code that is to be used by the EVM.
 		// The contract is a scoped environment for this execution context only.
@@ -454,7 +456,9 @@ func (evm *EVM) DelegateCall(originCaller common.Address, caller common.Address,
 
 	// It is allowed to call precompiles, even via delegatecall
 	if p, isPrecompile := evm.precompile(addr); isPrecompile {
+		ptime := time.Now()
 		ret, gas, err = RunPrecompiledContract(p, input, gas, evm.Config.Tracer)
+		evm.Stats.recordPrecompile(ptime)
 	} else {
 		// Initialise a new contract and make initialise the delegate values
 		//
@@ -506,7 +510,9 @@ func (evm *EVM) StaticCall(caller common.Address, addr common.Address, input []b
 	evm.StateDB.AddBalance(addr, new(uint256.Int), tracing.BalanceChangeTouchAccount)
 
 	if p, isPrecompile := evm.precompile(addr); isPrecompile {
+		ptime := time.Now()
 		ret, gas, err = RunPrecompiledContract(p, input, gas, evm.Config.Tracer)
+		evm.Stats.recordPrecompile(ptime)
 	} else {
 		// Initialise a new contract and set the code that is to be used by the EVM.
 		// The contract is a scoped environment for this execution context only.
